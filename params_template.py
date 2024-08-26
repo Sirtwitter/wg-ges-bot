e0a974c4d3d32824e0aa2dcb79bc05508d3122a5
Sirtwitter <85166895+Sirtwitter@users.noreply.github.com>
2024-08-26 20:35:19 +0200

    Update params_template.py

--- params_template.py
+++ params_template.py
@@ -1,3 +1,3 @@
-admin_chat_id = 123456789  # Get your user id, e.g. ask @FalconGate_Bot /get_my_id
+admin_chat_id = 979616208  # Get your user id, e.g. ask @FalconGate_Bot /get_my_id
 tor_pwd = None  # None is default. If you set one up at your tor config you need to provide it here
-token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"  # Get your token by chatting with Telegram's @BotFather
+token = "7524478392:AAGeZt5sf_mauoVyLE0vQQXf6bs5dj2h5ao"  # Get your token by chatting with Telegram's @BotFather
